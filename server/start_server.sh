44efdbac098bdf3960e00b5080445f3feed8b033
Honza Kosák <kosak@webovy-servis.cz>
2019-01-07 21:41:37 +0100

    initial commit

---
+++ server/start_server.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+printf " #########################################\n"
+printf " ######### COD4 Dedicated Server #########\n"
+printf " #########################################\n\n"
+
+printf "used arguments: $@\n\n"
+
+cd /cod4
+./cod4_lnxded "$@"
+
+status=$?
+
+printf " Exited with status $status\n"
+printf " #########################################\n"
