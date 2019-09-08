acbd10aeac17b16b3b9c281628c097b31fe5d970
root <root@kickme.cz>
2019-09-08 11:47:39 +0000

    fix permissions, move server.cfg

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+cd /srv/cod4
+
+printf "fixing permissons..."
+chown -R cod4 .
+chmod 700 .
+chmod -R 400 ./*
+chmod 500 ./entrypoint.sh ./cod4_lnxded
+chmod -R 700 ./main ./mods
+
 printf " #########################################\n"
 printf " ######### COD4 Dedicated Server #########\n"
 printf " #########################################\n\n"
